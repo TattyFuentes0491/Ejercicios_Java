88b009e105c4caabb15b1708e8155eb0ec982fcd
Tatiana Fuentes <58365448+TattyFuentes0491@users.noreply.github.com>
2021-12-10 10:51:05 -0500

    Update Ejercicio2_TableroDamas.java

--- Ejercicio2_TableroDamas.java
+++ Ejercicio2_TableroDamas.java
@@ -1,17 +1,4 @@
-/*
-Pregunta 2
-Imprimir un tablero de damas donde la "X" representa el color negro y el "_" representa el blanco. 
-El tablero debe tener n x n casillas. Por ejemplo, para n=5 el tablero se vería así:
-X_X_X
-_X_X_
-X_X_X
-_X_X_
-X_X_X
-Tu tablero siempre debe partir con un cuadro negro (una "X") en la esquina superior izquierda y el valor de n puede ir 
-de 1 a 10. En caso de que el valor de n sea diferente, asumir que n es igual a 5.
-El código para el tamaño de n ya está ahí, puede editarlo para probar con otros valores y puede hacer clic en el botón 
-de actualización junto a él para volver al valor original que se utiliza para validar su código durante la prueba. 
- */
+
 package prueba;
 
 /**
